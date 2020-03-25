5788d96c116f7430d66ed53ac92c2f66b2b333e7
Mr-G00D <60680315+Mr-G00D@users.noreply.github.com>
2020-03-25 16:33:10 +0530

    Delete mrgood.py

--- mrgood.py
+++
@@ -1,7 +0,0 @@
-#fun_python#
-This python program uses while loop to continuosly run a command line...and the fun part is where you gonna find what's inside the program......
-
-
-
-
-Just run it or inspect it... it's worth it folks.
